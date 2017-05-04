87d4d7915badc0d5a914b5f834b80877044ca7ef
Duoc <duoc@construct.sg>
2017-05-05 02:55:18 +0700

    update about page

--- src/components/main/about.vue
+++ src/components/main/about.vue
@@ -8,11 +8,11 @@
 					</div>
 					<div class="page-body">
 						<p>Hello friends.</p>
-						<p>Do you like Gradient Hero? Can Gradient Hero help you with your work?</p>
+						<p>Do you like Gradient Hero? Did Gradient Hero helpful in your work?</p>
 						<p>I did it during my international labor holiday. I really feel comfortable and happy to be coding what I like, not depending on anything.</p>
 						<p>The purpose of building this website is that I want to save beautiful gradients for later use and also share with you.</p>
 						<p>I am using the free package of Google's Firebase server. Can only accommodate 100 visitors at the same time. So it will not able to serve everyone. This site needs funding. If you feel like contributing to the community, donate at Paypal: <a target="_blank" href="https://paypal.me/duocnguyen">Donate now</a></p>
-						<p>Actually, this site is my own design and also code itself. I like both of these things. If you have any suggestion: Please contact me at <a href="mailto:nguyenvanduocit@gmail.com">nguyenvanduocit@gmail.com</a></p>
+						<p>Actually, this site was designed and coded by itself. I like both of these things. If you have any suggestion: Please contact me at <a href="mailto:nguyenvanduocit@gmail.com">nguyenvanduocit@gmail.com</a></p>
 					</div>
 				</div>
 			</div>
